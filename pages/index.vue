a5f7e26cda041de2e8f5cddc2346a9d7f4d08623
kaivanwong <kaivanwong@outlook.com>
2023-02-23 09:45:08 +0800

    add index.vue

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+useHead({
+  title: 'Kaivan Wong',
+})
+</script>
+
+<template>
+  <article>
+    <div style="display: none;">
+      <span i-simple-icons-github />
+      <span i-simple-icons-twitter />
+      <span i-simple-icons-microsoftoutlook />
+    </div>
+    <ContentDoc class="prose m-auto">
+      <template #not-found>
+        <h2>Ooops!</h2>
+        <p>Sorry, the page you are looking for could not be found.</p>
+      </template>
+    </ContentDoc>
+  </article>
+</template>
