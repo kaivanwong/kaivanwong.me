86bae78e464384015b1070b15b11c2d68205c27b
kaivanwong <kaivanwong@outlook.com>
2023-08-22 10:37:44 +0800

    feat: add links for footer

--- components/copyright.vue
+++ components/copyright.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const route = useRoute()
+</script>
+
 <template>
   <footer mt-10 mb-6 prose m-auto opacity-50>
     <p text-sm>
@@ -10,6 +14,14 @@
     <p text-sm>
       <a title="License" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" mr-2>CC BY-NC-SA 4.0</a>
       <span>© 2022-PRESENT Kaivan Wong</span>
+    </p>
+    <p v-if="route.path !== '/'" text-sm flex items-center grid gap-4>
+      <span>Links: </span>
+      <a title="Github" href="https://github.com/kaivanwong" target="_blank" i-simple-icons-github />
+      <a title="Twitter" href="https://twitter.com/kaivanwong" target="_blank" i-simple-icons-twitter />
+      <a title="Instagram" href="https://github.com/hikaivanwong" target="_blank" i-simple-icons-instagram />
+      <a title="Zhihu" href="https://www.zhihu.com/people/kaivanwong" target="_blank" i-simple-icons-zhihu />
+      <a title="Bilibili" href="https://space.bilibili.com/190014206" target="_blank" i-simple-icons-bilibili />
     </p>
   </footer>
 </template>
