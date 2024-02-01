11abe7f11bc464213b5130d0f24f7bce33dea797
kaivanwong <kaivanwong@outlook.com>
2024-02-01 11:35:42 +0800

    feat: update favicon

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+// For more information on configuration options, see:
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
+    server:{
+        port: 1977
+    },
     integrations: [
         mdx(),
         sitemap(),
