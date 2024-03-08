f4585e47fb9c122753ccef7ae0765df0d8bd2491
kaivanwong <kaivanwong@outlook.com>
2024-03-08 13:51:02 +0800

    fix: remove mt style

--- src/components/PageOperate.vue
+++ src/components/PageOperate.vue
@@ -11,7 +11,7 @@ const props = withDefaults(defineProps<{
 </script>
 
 <template>
-  <div mt-8>
+  <div>
     <div v-if="showSourceLink" flex="~ items-center" text-main opacity-50 font-mono>
       <span mr-2 text-lg>></span>
       <a prose-link :href="`https://github.com/kaivanwong/kaivanwong.me/src/content${props.pathname}`">Open Source in GitHub</a>
