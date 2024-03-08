9446ed24fc15e7e9795528b6da5032bff0f350a7
kaivanwong <kaivanwong@outlook.com>
2024-03-08 15:30:29 +0800

    feat: update operate share link

--- src/components/PageOperate.vue
+++ src/components/PageOperate.vue
@@ -1,23 +1,37 @@
 <script lang="ts" setup>
-const props = withDefaults(defineProps<{
-  showSourceLink?: boolean
+withDefaults(defineProps<{
+  showShare?: boolean
   showBack?: boolean
-  pathname?: string
+  url?: URL
 }>(), {
-  showSourceLink: false,
+  showShare: false,
   showBack: true,
-  pathname: '',
 })
+
+const shareLinks = [
+  {
+    text: 'Twitter',
+    icon: 'i-simple-icons-x',
+    href: 'https://twitter.com/intent/tweet?url=',
+  },
+  {
+    text: 'Mail',
+    href: 'mailto:?subject=See%20this%20post&body=',
+  },
+]
 </script>
 
 <template>
   <div>
-    <div v-if="showSourceLink" flex="~ items-center" text-main opacity-50 font-mono>
-      <span mr-2 text-lg>></span>
-      <a prose-link :href="`https://github.com/kaivanwong/kaivanwong.me/src/content${props.pathname}`">Open Source in GitHub</a>
+    <div v-if="showShare" flex="~ gap-4 items-start flex-wrap" text-main opacity-50 font-mono>
+      <span text-lg>></span>
+      <span>share to</span>
+      <a v-for="link in shareLinks" :key="link.text" prose-link :href="link.href + url">
+        <i v-if="link.icon" text-2.8 :class="link.icon" mr-0.8 />{{ link.text }}
+      </a>
     </div>
-    <div v-if="showBack" flex="~ items-center" text-main opacity-50 font-mono>
-      <span mr-2 text-lg>></span>
+    <div v-if="showBack" flex="~ gap-4 items-center " text-main opacity-50 font-mono>
+      <span text-lg>></span>
       <a prose-link href="javascript:history.back(-1)">cd ..</a>
     </div>
   </div>
