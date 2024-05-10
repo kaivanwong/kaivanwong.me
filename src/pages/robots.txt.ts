2b199b0a55b6ae955d77fcb57ebe33cb742496e8
kaivanwong <kaivanwong@outlook.com>
2024-05-10 17:35:01 +0800

    fix: fix header

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,11 +4,10 @@ interface Context {
 
 export function GET(context: Context) {
   const robots = `
-  User-agent: *
-  Allow: /
+User-agent: *
+Allow: /
   
-  Sitemap: ${new URL('sitemap-index.xml', context.site).href}
-  `.trim()
+Sitemap: ${new URL('sitemap-index.xml', context.site).href}`.trim()
 
   return new Response(robots, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
